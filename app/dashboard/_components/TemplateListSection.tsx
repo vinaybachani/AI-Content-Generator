7d6b27c8cfadd21b6a3e4f50734ebdf9e91950b5
vinaybachani <blikevinay@gmail.com>
2024-11-09 17:58:37 +0530

    made some changes in ui

--- app/dashboard/_components/TemplateListSection.tsx
+++ app/dashboard/_components/TemplateListSection.tsx
@@ -33,7 +33,7 @@ const TemplateListSection = ({ userSearchInput }: any) => {
         }
     }, [userSearchInput]);
     return (
-        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 p-10'>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 p-10'>
             {templateList.map((item: TEMPLATE, index: number) => (
                 <TemplateCard key={index} {...item} />
             ))}
